40ce3f915d3ca2cb2de975b22dcf3decc3460e52
johnnam1121 <johnnam93@gmail.com>
2023-09-26 15:19:55 -0500

    working on carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
 import Image from 'next/image';
-import pbp from '../public/images/homepage/pbp-1024x429.png';
-import ourProcess from '../public/images/homepage/OurProcess.png';
-import Specialties from '../public/images/homepage/Specialties.png';
-import referral from '../public/images/homepage/referral.png';
-import phone from '../public/images/homepage/phone.png';
+import pbp from '../public/images/Homepage/pbp-1024x429.png';
+import ourProcess from '../public/images/Homepage/OurProcess.png';
+import Specialties from '../public/images/Homepage/Specialties.png';
+import referral from '../public/images/Homepage/referral.png';
+import phone from '../public/images/Homepage/phone.png';
+import ImageCarousel from './components/ImageCarousel';
 
 export default function Home() {
   return (
     <main>
       {/* pbp landing */}
-      <div className="h-screen bg-primary bg-cover bg-no-repeat xl:bg-[url('../public/images/homepage/WH-hdr-11.png')]">
-        <div className="h-full p-10 flex items-center">
-          <div className="max-w-3xl xl:ml-60 mx-auto">
-            <Image src={pbp} alt='Powered By People' width={800} height={400} />
-            <div className='mt-10 border-t-4 border-lightBlue'>
-              <p className="block mt-10 font-semibold text-md">At our core, WeHire Group wants to make a difference in people&apos;s careers. We provide professionals with the tools to achieve their goals and connect clients with motivated individuals that impact their business.</p>
-            </div>
+      <div className="pt-20 bg-primary bg-cover bg-no-repeat xl:bg-[url('../public/images/Homepage/WH-hdr-11.png')]">
+        <div className="h-full p-10 flex items-center mt-20">
+          <div className='mx-auto lg:w-5/12 my-10 lg:ml-32'>
+            <Image src={pbp} alt='we hire big 4 logo' width={600} height={400} />
+            <hr className='mt-10 border-t-4 border-lightBlue' />
+            <p className="text-secondary block mt-10 font-semibold text-lg">At our core, WeHire Group wants to make a difference in
+              people&apos;s careers. We provide professionals with the tools to achieve their goals and connect clients with motivated
+              individuals that impact their business.</p>
           </div>
         </div>
       </div>
@@ -88,9 +90,9 @@ export default function Home() {
           <hr className="my-12 border-secondary border-2 dark:border-secondary" />
           <p className=' text-secondary font-semibold text-md'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
         </div>
-        {/* <p className='text-secondary font-semibold text-md'>consulting</p>
-        <p className='text-secondary font-semibold text-md'>big4</p>
-        <p className='text-secondary font-semibold text-md'>tax</p> */}
+        <div>
+          <ImageCarousel />
+        </div>
       </div>
 
       {/* Referral Bonus */}
